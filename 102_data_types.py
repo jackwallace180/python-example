938949c575e10e880fc8de40b3ed14ab4d3a2bdc
Filipe-p <filipe.p@soltroiavillas.com>
2019-09-23 11:03:25 +0100

    finished numericals, moving into booleans

---
+++ 102_data_types.py
@@ -0,0 +1,53 @@
+# Numerical Data types
+# - Int, long, float, complex
+# These are numerical data types which we can use numerical operators.
+
+# Complex and Long we don't use as much
+# Complex bring an imaginary type of number
+# long - are integers of unlimited size
+#
+
+# int - Stands for integers
+# Whole numbers
+my_int = 10
+print(my_int)
+print(type(my_int))
+
+# Opperator - Add, Subtract, devide and multiply
+  # Exponential
+print(4 + 3)
+print(4 - 3)
+print(4/2) # Devisions automatically get converted to float
+print(4//2) # Keeps it as interger / drops the decimal
+print(10/3)
+print(5//2)
+print(4*2)
+print(3**2)
+
+#Modules looks for the number of remainder
+# % ##
+print(10%3) ## --> 3*3 = 9, so 1 is the remainder
+print(22%3) ## --> 3*7 = 21, so 1 is the remainder
+print(23%3) ## --> 3*7 = 21, so 2 is the remainder
+
+# Comparission Operators # --> boolean value
+
+#  ==  - Comparission Operator
+#  < / >  - Bigger and smaller than
+#  <=  - Smaller than or equal
+#  >=  - Bigger than or equal
+#  !=  - Not equal
+# is and is not
+
+my_variable1 = 10
+my_variable2 = 13
+
+print(my_variable1 == my_variable2)
+print(my_variable1 > my_variable2)
+print(my_variable1 < my_variable2)
+print(my_variable2 >= 13)
+print(my_variable2 is 13)
+print(my_variable2 is not 13)
+
+
+# Boolean Values
