8276ca59eb91bf7a886ce9a402e33c7fb014b86a
Filipe-p <filipe.p@soltroiavillas.com>
2019-09-23 11:48:32 +0100

    exploring string and variables and others

--- 102_data_types.py
+++ 102_data_types.py
@@ -1,53 +1,82 @@
-# Numerical Data types
-# - Int, long, float, complex
-# These are numerical data types which we can use numerical operators.
-
-# Complex and Long we don't use as much
-# Complex bring an imaginary type of number
-# long - are integers of unlimited size
+# # Numerical Data types
+# # - Int, long, float, complex
+# # These are numerical data types which we can use numerical operators.
 #
+# # Complex and Long we don't use as much
+# # Complex bring an imaginary type of number
+# # long - are integers of unlimited size
+# #
+#
+# # int - Stands for integers
+# # Whole numbers
+# my_int = 10
+# print(my_int)
+# print(type(my_int))
+#
+# # Opperator - Add, Subtract, devide and multiply
+#   # Exponential
+# print(4 + 3)
+# print(4 - 3)
+# print(4/2) # Devisions automatically get converted to float
+# print(4//2) # Keeps it as interger / drops the decimal
+# print(10/3)
+# print(5//2)
+# print(4*2)
+# print(3**2)
+#
+# #Modules looks for the number of remainder
+# # % ##
+# print(10%3) ## --> 3*3 = 9, so 1 is the remainder
+# print(22%3) ## --> 3*7 = 21, so 1 is the remainder
+# print(23%3) ## --> 3*7 = 21, so 2 is the remainder
+#
+# # Comparission Operators # --> boolean value
+#
+# #  ==  - Comparission Operator
+# #  < / >  - Bigger and smaller than
+# #  <=  - Smaller than or equal
+# #  >=  - Bigger than or equal
+# #  !=  - Not equal
+# # is and is not
+#
+# my_variable1 = 10
+# my_variable2 = 13
+#
+# print(my_variable1 == my_variable2)
+# print(my_variable1 > my_variable2)
+# print(my_variable1 < my_variable2)
+# print(my_variable2 >= 13)
+# print(my_variable2 is 13)
+# print(my_variable2 is not 13)
+#
+#
+# # Boolean Values
+# # Defined by wither True or False
+# print(type(True))
+# print(type(False))
+# print(0 == False)
+# print(1 == True)
+#
+## None
+print(None)
+print(type(None))
+print(bool(None))
+print(0 == None)
+print(False == None)
+print(False == bool(None))
 
-# int - Stands for integers
-# Whole numbers
-my_int = 10
-print(my_int)
-print(type(my_int))
+print('Logical and & or ------------')
+# Logical AND & OR
+a = True
+b = False
+# using *and* both sides have to be true for it to result in true
+print(a and True)
+print((1 == 1) and (True))
+print((1==1) and False)
 
-# Opperator - Add, Subtract, devide and multiply
-  # Exponential
-print(4 + 3)
-print(4 - 3)
-print(4/2) # Devisions automatically get converted to float
-print(4//2) # Keeps it as interger / drops the decimal
-print(10/3)
-print(5//2)
-print(4*2)
-print(3**2)
-
-#Modules looks for the number of remainder
-# % ##
-print(10%3) ## --> 3*3 = 9, so 1 is the remainder
-print(22%3) ## --> 3*7 = 21, so 1 is the remainder
-print(23%3) ## --> 3*7 = 21, so 2 is the remainder
-
-# Comparission Operators # --> boolean value
-
-#  ==  - Comparission Operator
-#  < / >  - Bigger and smaller than
-#  <=  - Smaller than or equal
-#  >=  - Bigger than or equal
-#  !=  - Not equal
-# is and is not
-
-my_variable1 = 10
-my_variable2 = 13
-
-print(my_variable1 == my_variable2)
-print(my_variable1 > my_variable2)
-print(my_variable1 < my_variable2)
-print(my_variable2 >= 13)
-print(my_variable2 is 13)
-print(my_variable2 is not 13)
-
-
-# Boolean Values
+# use or, ONLY 1 side needs to be true
+print('this will print true')
+print(True or False)
+print(True or 1 == 2)
+print('this will print False')
+print(False or 1 == 2)
---
+++ 103_data_types_strings.py
@@ -0,0 +1,54 @@
+# String
+# these are a list of characters put together
+# defined by '' or ""
+
+# my_string = 'Amazing Grilled Fish'
+# print(type(my_string))
+# # you can print it
+# print(my_string)
+
+# Join of strings - concatenation
+first_name = 'Boris'
+last_name = 'Becker'
+print(first_name[0]) # sting IS a list of characters. therefore behaves like a list
+# concatenating 3 strings
+full_name = first_name + ' ' + last_name
+print(full_name)
+print('string', 14)
+
+#Interpolation
+name = 'Rachel'
+welcome_message = 'hey there, how youuu doin? ;)'
+print(f'hey there, {name} how youuu doin? ;)')
+# place an f on the left/beginning of the string
+# then you can use {} to interpolated python variables inside
+
+
+# Useful Methods for strings
+my_string = ' HEllo this is an amazing string    '
+
+#.count()
+print(my_string.count('i'))
+print(my_string.count(' '))
+
+#.strip()
+print(my_string.strip())
+
+#len() # Not a method - general method built in
+print(len(my_string))
+
+#.lower()
+print(my_string.lower())
+
+#.upper()
+print(my_string.upper())
+
+#.capitalize()
+print(my_string.strip().capitalize())
+
+#.replace(arg_int, arg_two)
+print(my_string.replace('an', 'THE'))
+
+#.split(arg) --> list
+print(my_string.split(' '))
+print(type(my_string.split(' ')))
