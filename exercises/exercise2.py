9f9f14ff4146aae5323b8ee2625b741ac3878443
Filipe-p <filipe.p@soltroiavillas.com>
2019-09-23 14:20:01 +0100

    second exercise completed

---
+++ exercises/exercise2.py
@@ -0,0 +1,19 @@
+# I want you to use operators
+# equate something
+
+# As a user, I want to be able to guess a number and know if i got it correct or not, so that I can know if I won or not.
+
+
+# We should define/assign number to a variable called magic_number
+magic_number = 7
+
+# I need to ask user for input
+magic_input = int(input("Please input your magic number: "))
+
+# I need to check if this input matches a magic_number
+print("Your guess was: "+ str( magic_number == magic_input))
+
+print("Your guess was:", magic_number == magic_input)
+
+# I need to let the user know if the response was write or not
+#self five
